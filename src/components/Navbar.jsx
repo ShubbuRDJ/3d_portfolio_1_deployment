63f67a50d286e0e19e9f77e94de7e2a267b99f6f
Shubham Singh <shubham.s@appventurez.com>
2024-12-31 04:05:49 +0530

    small changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
           window.scrollTo(0, 0);
         }}>
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">Shubham &nbsp;<span className='sm:block hidden sm:hidden lg:block xl:block'>|&nbsp;Full Stack Developer</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Shubham &nbsp;<span className='hidden sm:hidden lg:block xl:block'>|&nbsp;Full Stack Developer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks?.map((link) => {
